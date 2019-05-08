34defc58b02300d3f4c44de0d806a84b45bda788
Ermeichook <ruslan.ermeichook@gmail.com>
2019-05-09 01:33:39 +0300

    make Intent

---
+++ app/src/main/java/com/example/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,22 @@
+package com.example.mymessenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE ="message" ;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.message);
+        messageView.setText(messageText);
+
+    }
+}
